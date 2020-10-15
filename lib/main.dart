49e2b52f0e5f5cc7905aac5126747408408e0abe
shivi <agarwalshivi082@gmail.com>
2020-10-15 14:09:45 +0530

    meal_details added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import './category_meals_screen.dart';
-import './categories_screen.dart';
+import './screens/meal_detail_screen.dart';
+import './screens/category_meals_screen.dart';
+import './screens/categories_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -33,6 +34,19 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (ctx) => CategoriesScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+      },
+      onGenerateRoute: (settings) {
+        print(settings.arguments);
+        /* if(settings.name == '/meal-detail'){
+          return ...;
+        } else if(settings.name == '/something-else'){
+          return ...;
+        }
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen()); */
+      },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
       },
     );
   }
